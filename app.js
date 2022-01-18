a3f48c5ea56037072dd665fe0beed64d4ffba830
jakhongiralijonov <jakhongirweb@gmail.com>
2022-01-18 10:45:29 +0500

    first commit

---
+++ app.js
@@ -0,0 +1,33 @@
+const form = document.querySelector('form')
+const words = document.querySelector('.word')
+const counter = document.querySelector('.counter')
+const input = document.querySelector('#input')
+mainFunc()
+let randomWord
+
+function mainFunc(main) {
+
+    fetch('https://random-word-api.herokuapp.com/word')
+        .then(function(data) {
+            return data.json()
+        })
+        .then(getWord)
+
+    function getWord(word) {
+        randomWord = word[0]
+        words.innerHTML = randomWord
+        input.value = ''
+
+    }
+}
+let count = 0;
+
+form.addEventListener('input', (e) => {
+    if (randomWord == input.value) {
+        mainFunc();
+
+        count++;
+        counter.innerHTML = `Your score: ${count}`
+    }
+
+})
